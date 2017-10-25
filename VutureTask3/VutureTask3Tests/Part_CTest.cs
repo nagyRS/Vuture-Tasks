58610e0c8a7c67d55f7fc453d0b08c375f6810f7
Reka Sara Nagy <nagyrs@gmail.com>
2017-10-25 07:37:27 +0100

    Add files via upload

---
+++ VutureTask3/VutureTask3Tests/Part_CTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VutureTask3Tests
+{
+    [TestClass ()]
+    class Part_CTest
+    {
+        [TestMethod]
+        public static void DoWorkTest()
+        {
+
+        }
+    }
+}
